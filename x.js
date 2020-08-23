42ddda570a836c3ffcb165f0d315e1649d59d11c
Mansi <mansiagnicse@gmail.com>
2020-08-23 22:48:37 +0530

    Unused CSS deleted

--- x.js
+++
@@ -1,9 +0,0 @@
-import purify from "purify-css"
-const purify = require("purify-css")
-
-let content = ""
-let css = ""
-let options = {
-    output: "filepath/output.css"
-}
-purify(content, css, options)
