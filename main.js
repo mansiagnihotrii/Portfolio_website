c9ee8fc3897f053e1a256335a38f3e3ba7b4f8c6
Mansi <mansiagnicse@gmail.com>
2020-10-18 19:32:40 +0530

    Code quality improved

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-if('serviceWorker' in navigator){
-window.addEventListener('load',() => {
-navigator.serviceWorker.register('/sw.js')
-.then(reg => {
-console.log('Registered!',reg);})
-.catch(err => {
-console.log("Failed: ",err);
-});
-});
+if ('serviceWorker'in navigator) {
+    window.addEventListener('load', ()=>{
+        navigator.serviceWorker.register('/sw.js').then(reg=>{
+            console.log('Registered!', reg);
+        }
+        ).catch(err=>{
+            console.log("Failed: ", err);
+        }
+        );
+    }
+    );
 }
--- sw.js
+++ sw.js
@@ -1,40 +1,29 @@
-self.addEventListener('install',event => {
-console.log('Install event!');
-});
+self.addEventListener('install', event=>{
+    console.log('Install event!');
+}
+);
 
-self.addEventListener('activate',event => {
-console.log('Activate event!');
-});
+self.addEventListener('activate', event=>{
+    console.log('Activate event!');
+}
+);
 
 self.addEventListener('fetch', function(event) {
- console.log(event.request.url);
+    console.log(event.request.url);
 
- event.respondWith(
-   caches.match(event.request).then(function(response) {
-     return response || fetch(event.request);
-   })
- );
+    event.respondWith(caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+    }));
 });
-
 
 const cacheName = 'cache-v1';
-const resourcesToPrecache = [
-	'/',
-	'index.html',
-	'style/style.css',
-	'img/blog.webp',
-	'img/dog.webp',
-	'img/epoller.webp',
-	'img/flashcards.webp',
-	'img/me.webp'
-];
+const resourcesToPrecache = ['/', 'index.html', 'style/style.css', 'img/blog.webp', 'img/dog.webp', 'img/epoller.webp', 'img/flashcards.webp', 'img/me.webp'];
 
-self.addEventListener('install',event => {
-console.log('Service worker install event!');
-event.waitUntil(
-	caches.open(cacheName)
-	.then(cache => {
-		return cache.addAll(resourcesToPrecache);
-	})
-  );
-});
+self.addEventListener('install', event=>{
+    console.log('Service worker install event!');
+    event.waitUntil(caches.open(cacheName).then(cache=>{
+        return cache.addAll(resourcesToPrecache);
+    }
+    ));
+}
+);
